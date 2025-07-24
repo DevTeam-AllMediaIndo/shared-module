8f97d19a532e44bceb0b105ca6373a3ea712a20f
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-24 07:10:52 +0700

    Update create.php

--- src/AdminPermission/views/admins/create.php
+++ src/AdminPermission/views/admins/create.php
@@ -10,7 +10,6 @@ if(!$isAllowToCreate) {
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="javascript:void(0);">Home</a></li>
             <li class="breadcrumb-item"><a href="javascript:void(0);">Admin</a></li>
-            <li class="breadcrumb-item"><a href="javascript:void(0);">List</a></li>
             <li class="breadcrumb-item active" aria-current="page"><a href="javascript:void(0);">Tambah</a></li>
         </ol>
     </div>
