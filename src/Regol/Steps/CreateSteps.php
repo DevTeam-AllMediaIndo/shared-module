31646e6e66fbb26b75cacf451160a17a17293cd5
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-24 21:53:31 +0700

    edit: menambahkan validasi array

--- src/Regol/Steps/CreateSteps.php
+++ src/Regol/Steps/CreateSteps.php
@@ -3,8 +3,21 @@ namespace Allmedia\Shared\Regol\Steps;
 
 class CreateSteps {
 
-    public static function create(array $array): array|bool {
-        return $array;
+    public static function create(array $array): array {
+        $steps = [];
+        foreach($array as $ar) {
+            $steps = self::validate($ar);
+        }
+
+        return $steps;
     }
 
+    public static function validate(array $data) {
+        /** Required Data */
+        $data['handler'] ??= "Invalid Handler";
+        $data['title'] ??= $data['handler'];
+        $data['data'] ??= [];
+
+        return $data;
+    }
 }
