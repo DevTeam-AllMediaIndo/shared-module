5041c80901f5671ab34cc8f32537f86e6bcc6b6a
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-24 11:14:33 +0700

    Delete src/AdminPermission/contracts directory

--- src/AdminPermission/contracts/ShareViewInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Allmedia\Shared\AdminPermission\Contracts;
-
-interface ShareViewInterface {
-    
-    public static function render(string $filepath, array $data = []);
-    public static function render_script(string $filepath, array $data = []);
-
-}
