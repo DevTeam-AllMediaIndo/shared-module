4ab54f843c75ad6da6d26f5378b25049e8e5d6c6
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-24 21:24:30 +0700

    add: template halaman regol

---
+++ src/Regol/views/index.php
@@ -0,0 +1,21 @@
+<div class="row">
+    <div class="col-12">
+        <div class="panel">
+            <div class="mt-2 mb-2 part text-center step-wizard" id="nav-tab" role="tablist">
+                <ul class="step-wizard-list">
+                    <li class="step-wizard-item">
+                        <span class="progress-count">
+                            <button type="button" onclick="location.href = `{$pageLink}`" class="btn btn-sm text-dark btn-outline-primary" id="-tab" aria-selected="false">
+                                Step
+                            </button>
+                        </span>
+                        <span class="progress-label">Step Name</span>
+                    </li>
+                </ul>
+            </div>
+            <div class="panel-body">
+                ini body
+            </div>
+        </div>
+    </div>
+</div>
