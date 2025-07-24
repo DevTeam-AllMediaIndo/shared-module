931834acdc3beab3cfffa8d971e930e95901db0a
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-25 02:29:46 +0700

    add: step create demo

--- src/Regol/Steps/StepCreateDemo.php
+++ src/Regol/Steps/StepCreateDemo.php
@@ -2,17 +2,44 @@
 namespace Allmedia\Shared\Regol\Steps;
 
 use Allmedia\Shared\Regol\Contracts\RegolStepInterface;
+use Exception;
 
 class StepCreateDemo implements RegolStepInterface {
 
-    public function handle(array $request): void {
+    public $pageTitle = "Buat Akun Demo";
+    public $pageView = "pembuatan-demo";
+    public $data = [];
+
+    public function __construct(array $data) {
+        $this->data = [
+            'fullname' => ($data['fullname'] ??= ""),
+            'email' => ($data['email'] ??= ""),
+            'phone' => ($data['phone'] ??= ""),
+            'date_create_demo' => ($data['date_create_demo'] ??= ""),
+            'demo' => [],
+        ];
+
+        $this->data['demo'] = [
+            'login' => $data['demo']['login'] ??= "",
+            'master' => $data['demo']['master'] ??= "",
+            'investor' => $data['demo']['investor'] ??= "",
+            'phone' => $data['demo']['phone'] ??= "",
+        ];
     }
 
-    public function validate(array $request): array|string {
-        return [];
+    public function validate(): bool {
+        return empty($this->data['demo']);
     }
 
-    public function view(): void {
+    public function view() {
+        try {
+            ob_start();
+            extract($this->data, EXTR_SKIP);
+            require_once __DIR__ . "/../views/pembuatan-demo.php";
+            return ob_get_clean();
 
+        } catch (Exception $e) {
+            throw $e;
+        }
     }
 }
