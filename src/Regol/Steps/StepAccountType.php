4014f4975fdc86c1ea757d75293d5fb7a4a37b80
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-25 02:30:03 +0700

    add: step rate & jenis akun

---
+++ src/Regol/Steps/StepAccountType.php
@@ -0,0 +1,45 @@
+<?php
+namespace Allmedia\Shared\Regol\Steps;
+
+use Allmedia\Shared\Regol\Contracts\RegolStepInterface;
+use Exception;
+
+class StepAccountType implements RegolStepInterface {
+
+    public $pageTitle = "Rate & Jenis Real Account";
+    public $pageView = "rate-jenis-account";
+    public $data = [];
+
+    public function __construct(array $data) {
+        $this->data = [
+            'fullname' => ($data['fullname'] ??= ""),
+            'email' => ($data['email'] ??= ""),
+            'phone' => ($data['phone'] ??= ""),
+            'date_create_demo' => ($data['date_create_demo'] ??= ""),
+            'demo' => [],
+        ];
+
+        $this->data['demo'] = [
+            'login' => $data['demo']['login'] ??= "",
+            'master' => $data['demo']['master'] ??= "",
+            'investor' => $data['demo']['investor'] ??= "",
+            'phone' => $data['demo']['phone'] ??= "",
+        ];
+    }
+
+    public function validate(): bool {
+        return empty($this->data['demo']);
+    }
+
+    public function view() {
+        try {
+            ob_start();
+            extract($this->data, EXTR_SKIP);
+            require_once __DIR__ . "/../views/pembuatan-demo.php";
+            return ob_get_clean();
+
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+}
