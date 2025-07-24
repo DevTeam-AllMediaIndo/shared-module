a07138bd14e8ebeb091e9b18ae1c80ec46297c15
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-24 07:00:58 +0700

    add: admin permission_button. update_button

---
+++ src/AdminPermission/views/admins/permission_button.php
@@ -0,0 +1,20 @@
+<?php if($isAllowToUpdate) : ?>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            if(table) {
+                table.on('draw.dt', function(evt) {
+                    $.each($('#table tbody tr'), (i, tr) => {
+                        let td = $(tr).find('td').eq(5);
+                        if(td) {
+                            let actionArea = td.find('.action');
+                            if(actionArea && !actionArea.find('.btn-permission').length) {
+                                let id = actionArea.data('id');
+                                actionArea.append(`<a href="/admin/permission/${id}" class="btn btn-primary btn-sm text-white btn-permission"><i class="fas fa-gear"></i></a>`)
+                            }
+                        }
+                    })
+                })
+            }
+        })
+    </script>
+<?php endif; ?>
---
+++ src/AdminPermission/views/admins/update_button.php
@@ -0,0 +1,20 @@
+<?php if($isAllowToUpdate) : ?>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            if(table) {
+                table.on('draw.dt', function(evt) {
+                    $.each($('#table tbody tr'), (i, tr) => {
+                        let td = $(tr).find('td').eq(5);
+                        if(td) {
+                            let actionArea = td.find('.action');
+                            if(actionArea && !actionArea.find('.btn-edit').length) {
+                                let id = actionArea.data('id');
+                                actionArea.append(`<a href="/admin/update/${id}" class="btn btn-success btn-sm text-white btn-edit me-1"><i class="fas fa-edit"></i></a>`)
+                            }
+                        }
+                    })
+                })
+            }
+        })
+    </script>
+<?php endif; ?>
