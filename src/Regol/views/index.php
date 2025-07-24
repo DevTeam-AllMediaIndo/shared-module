78b41f28c78991169a1cad3dcfe9ce004668b317
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-25 02:30:06 +0700

    Update index.php

--- src/Regol/views/index.php
+++ src/Regol/views/index.php
@@ -1,20 +1,24 @@
+<link rel="stylesheet" href="/assets/css/regol.css">
 <div class="row">
     <div class="col-12">
         <div class="panel">
             <div class="mt-2 mb-2 part text-center step-wizard" id="nav-tab" role="tablist">
                 <ul class="step-wizard-list">
-                    <li class="step-wizard-item">
-                        <span class="progress-count">
-                            <button type="button" onclick="location.href = `{$pageLink}`" class="btn btn-sm text-dark btn-outline-primary" id="-tab" aria-selected="false">
-                                Step
-                            </button>
-                        </span>
-                        <span class="progress-label">Step Name</span>
-                    </li>
+                    <?php foreach($steps as $key => $step) : ?>
+                        <?php $i = $key + 1; ?>
+                        <li class="step-wizard-item">
+                            <span class="progress-count">
+                                <button type="button" onclick="location.href = `{$pageLink}`" class="btn btn-sm text-dark btn-outline-primary" id="tab1" aria-selected="false" title="test">
+                                    <?= $i ?>
+                                </button>
+                            </span>
+                            <span class="progress-label"><?= $step->pageTitle ?></span>
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
             </div>
             <div class="panel-body">
-                ini body
+                <?= $content ?>
             </div>
         </div>
     </div>
