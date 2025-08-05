1c9d9d19c778fd08a59956cffe739071c053feef
Fani Annur Rozi <crazyhx4@gmail.com>
2025-08-05 20:09:25 +0700

    Update rate-jenis-account.php

--- src/Regol/views/rate-jenis-account.php
+++ src/Regol/views/rate-jenis-account.php
@@ -27,20 +27,20 @@
                 <div class="card-body">
                     <div class="mb-3">
                         <label class="form-label">Demo Account</label>
-                        <input type="text" class="form-control" value="<?= $demoAccount['ACC_LOGIN'] ?? ""; ?>" disabled>
+                        <input type="text" class="form-control" value="<?= $demo_login ?? ""; ?>" disabled>
                     </div>
 
                     <div class="mb-3">
                         <label class="form-label">Account Type</label>
                         <select id="acc-type" class="form-select mb-3">
-                            <?php foreach($accountCategories as $accType) : ?>
-                                <option data-type="<?= strtolower($accType) ?>" value="<?= strtoupper($accType) ?>" <?= strtoupper($realAccount['RTYPE_TYPE_AS']) == strtoupper($accType)? "selected" : ""; ?>><?= $accType; ?></option>
+                            <?php foreach($categories as $key => $category) : ?>
+                                <option data-type="<?= strtolower($key) ?>" value="<?= strtoupper($key) ?>" <?= strtoupper($real_category) == strtoupper($key)? "selected" : ""; ?>><?= strtoupper($key); ?></option>
                             <?php endforeach; ?>
                         </select>
-                        
-                        <?php foreach($accountCategories as $type) : ?>
-                            <?php $lowerType = strtolower($type) ?>
-                            <div class="tab-categories" id="nav-<?= $lowerType ?>" <?= $lowerType != strtolower($realAccount['RTYPE_TYPE_AS'])? "style='display: none;'" : ""; ?>>
+
+                        <?php foreach($categories as $type_as => $types) : ?>
+                            <?php $lowerType = strtolower($type_as) ?>
+                            <div class="tab-categories" id="nav-<?= $lowerType ?>" <?= $lowerType != strtolower($real_category)? "style='display: none;'" : ""; ?>>
                                 <nav class="mb-3">
                                     <?php if($lowerType == "multilateral") : ?>
                                         <div class="alert alert-warning">
@@ -49,36 +49,36 @@
                                     <?php endif; ?>
 
                                     <div class="btn-box d-flex flex-wrap gap-2" id="nav-tab" role="tablist">
-                                        <?php foreach($accountTypes[ $lowerType ] ?? [] as $key => $category) : ?>
+                                        <?php foreach($types as $key => $category) : ?>
                                             <button class="btn btn-sm btn-outline-primary <?= $key == 0? "active" : ""; ?>" id="<?= $lowerType.$category['type'] ?>-tab" data-bs-toggle="tab" data-bs-target="#tab-<?= $lowerType.$category['type'] ?>" type="button" role="tab" aria-controls="tab-<?= $lowerType.$category['type'] ?>" aria-selected="<?= $key == 0? "true" : "false"; ?>"><?= strtoupper($category['type']) ?></button>
                                         <?php endforeach; ?>
                                     </div>
                                 </nav>
     
                                 <div class="tab-content profile-edit-tab">
-                                    <?php foreach($accountTypes[ $lowerType ] ?? [] as $key => $category) : ?>
+                                    <?php foreach($types ?? [] as $key => $category) : ?>
                                         <div class="tab-pane fade <?= $key == 0? "show active" : ""; ?>" id="tab-<?= $lowerType.$category['type'] ?>" role="tabpanel" aria-labelledby="tab-<?= $lowerType.$category['type'] ?>" tabindex="0">
                                             <div class="row">
                                                 <?php foreach($category['products'] as $accType) : ?>
-                                                    <?php if(strtoupper($accType['RTYPE_TYPE_AS']) == strtoupper($type)) : ?>
+                                                    <?php if(strtoupper($accType['type_as']) == strtoupper($type_as)) : ?>
                                                         <div class="col-md-4">
-                                                            <input type="radio" name="account-type" id="<?= $accType['RTYPE_SUFFIX'] ?>" value="<?= $accType['RTYPE_SUFFIX'] ?>" data-category="<?= $category['type'] ?>" <?= ($realAccount['ACC_TYPE'] == $accType['ID_RTYPE'])? "checked" : ""; ?>>
-                                                            <label for="<?= $accType['RTYPE_SUFFIX'] ?>" class="w-100 h-100 select-type">
+                                                            <input type="radio" name="account-type" id="<?= $accType['suffix'] ?>" value="<?= $accType['suffix'] ?>" data-category="<?= $category['type'] ?>" <?= ($real_rtype == $accType['type'])? "checked" : ""; ?>>
+                                                            <label for="<?= $accType['suffix'] ?>" class="w-100 h-100 select-type">
                                                                 <div class="file-manager-card">
                                                                     <div class="top">
                                                                         <div class="part-icon">
-                                                                            <span><?= strtoupper($accType['RTYPE_TYPE']) ?></span>
+                                                                            <span><?= strtoupper($accType['name']) ?></span>
                                                                         </div>
                                                                     </div>
                                                                     <div class="bottom">
                                                                         <div class="left">
-                                                                            <a class="folder-name"><?= $accType['RTYPE_NAME'] ?></a>
-                                                                            <span class="file-quantity mb-1"><?= ($accType['RTYPE_CURR'] == "IDR")? "Rate IDR " . $accType['RTYPE_RATE'] : "Floating"; ?></span>
-                                                                            <span class="file-quantity mb-1">Leverage: <?= $accType['RTYPE_LEVERAGE'] ?></span>
-                                                                            <span class="file-quantity">Commission: $<?= $accType['RTYPE_KOMISI'] ?></span>
+                                                                            <a class="folder-name"><?= $accType['name'] ?></a>
+                                                                            <span class="file-quantity mb-1"><?= ($accType['currency'] == "IDR")? "Rate IDR " . $accType['rate'] : "Floating"; ?></span>
+                                                                            <span class="file-quantity mb-1">Leverage: <?= $accType['leverage'] ?></span>
+                                                                            <span class="file-quantity">Commission: $<?= $accType['commission'] ?></span>
                                                                         </div>
                                                                         <div class="right">
-                                                                            <span class="storage-used"><?= $accType['RTYPE_CURR'] ?></span>
+                                                                            <span class="storage-used"><?= $accType['currency'] ?></span>
                                                                         </div>
                                                                     </div>
                                                                 </div>
@@ -97,7 +97,7 @@
 
                 <div class="card-footer">
                     <div class="d-flex flex-row justify-content-end align-items-center gap-2 mt-25">
-                        <a href="<?= ($prevPage['page'])? ("/create-acc/".$prevPage['page']) : "javascript:void(0)"; ?>" class="btn btn-secondary">Previous</a>
+                        <a href="<?= $pagePrev ?>" class="btn btn-secondary">Previous</a>
                         <button type="submit" class="btn btn-primary">Next</button>
                     </div>
                 </div>
@@ -117,7 +117,7 @@
             $('input[name="account-type"]').prop('checked', false)
             $('.tab-categories').hide();
             $(`#nav-${type}`).show();
-        });
+        }).change();
 
         $('#form-account-type').on('submit', function(event){
             event.preventDefault();
@@ -130,10 +130,10 @@
                 }
             })
 
-            $.post("/ajax/regol/accountType", data, function(resp) {
+            $.post("/ajax/post/account/regol/rate-jenis-account", data, function(resp) {
                 Swal.fire(resp.alert).then(function() {
                     if(resp.success) {
-                        location.href = resp.redirect
+                        location.href = resp.data.redirect
                     }
                 })
             }, 'json')
