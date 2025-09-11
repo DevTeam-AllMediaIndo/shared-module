772d9073c4b69011eb16bab5c098d7cb73ed1cf5
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-11 15:00:04 +0700

    edit: menambahkan variable untuk operation (order send)

--- src/Metatrader/ApiVariable.php
+++ src/Metatrader/ApiVariable.php
@@ -4,5 +4,11 @@ namespace Allmedia\Shared\Metatrader;
 class ApiVariable {
 
     public static array $timeframes = ['M1', 'M5', 'M15', 'M30', 'H1', 'H4'];
+    public static string $operationBuy = 'buy';
+    public static string $operationSell = 'sell';
+
+    public static function operations(): array {
+        return [self::$operationBuy, self::$operationSell];
+    }
 
 }
