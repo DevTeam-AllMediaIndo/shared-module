8ea225902901fe769d251bca151cd389aaea6224
Fani Annur Rozi <crazyhx4@gmail.com>
2026-02-04 01:25:36 +0700

    feat: memperbarui nilai operation buyLimit, sellLimit, buyStop, sellStop

--- src/Metatrader/ApiVariable.php
+++ src/Metatrader/ApiVariable.php
@@ -6,10 +6,10 @@ class ApiVariable {
     public static array $timeframes = ['M1', 'M5', 'M15', 'M30', 'H1', 'H4'];
     public static string $operationBuy = 'buy';
     public static string $operationSell = 'sell';
-    public static string $operationBuyLimit = 'buy_limit';
-    public static string $operationSellLimit = 'sell_limit';
-    public static string $operationBuyStop = 'buy_stop';
-    public static string $operationSellStop = 'sell_stop';
+    public static string $operationBuyLimit = 'buylimit';
+    public static string $operationSellLimit = 'selllimit';
+    public static string $operationBuyStop = 'buystop';
+    public static string $operationSellStop = 'sellstop';
 
     public static function operations(): array {
         return [self::$operationBuy, self::$operationSell, self::$operationBuyLimit, self::$operationSellLimit, self::$operationBuyStop, self::$operationSellStop];
