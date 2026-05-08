7058171c8043133137094b7f7517161a756278d7
Fani Annur Rozi <crazyhx4@gmail.com>
2026-05-08 20:03:50 +0700

    feat: add upload_custom method for handling custom file uploads

--- src/FileUpload/UploadAWS.php
+++ src/FileUpload/UploadAWS.php
@@ -236,6 +236,40 @@ class UploadAWS
         }
     }
 
+    public function upload_custom(array $files, string $file_prefix = "upload") {
+        try {
+            if(empty($files) || $files['error'] != 0) {
+                return $error_messages[ $files['error'] ] ?? "[ERROR] Upload file gagal";
+            }
+
+            /** file info */
+            $file_info = pathinfo($files['name']);
+            $mime = mime_content_type($files['tmp_name']);
+            if($mime === FALSE) {
+                return "Invalid File Type";
+            }
+
+            $upload = $this->upload(new CURLFile($files['tmp_name'], $mime, $files['name']));
+            if(!is_array($upload) || !array_key_exists("filename", $upload)) {
+                return $upload ?? "Failed to upload image";
+            }
+
+            $default = [
+                'size' => $files['size'],
+                'extension' => $file_info['extension'],
+                'mime' => $mime,
+            ];
+
+            return array_merge($default, $upload);
+
+        } catch (Exception $e) {
+            return $e->getMessage();
+            
+        } catch (S3Exception $s3) {
+            return $s3->getAwsErrorMessage();
+        }
+    }
+
     private function upload(CURLFile $file) {
         try {
             if(empty($this->folder)) {
